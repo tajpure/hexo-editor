af39651275d0fecd5fee56838e45bf04743d2b93
tajpure <tajpure@gmail.com>
2015-06-25 22:02:17 +0800

    add login

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -27,6 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ username: null, saveUninitialized: true, secret: 'keyboard cat', resave: true, cookie: { maxAge: 60000 }}));
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login');
+  var username = req.session.username;
+  if (username == null) {
+    res.render('login');
+  } else {
+    res.render('index');
+  }
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log(username);
+  req.session.username = username;
+  res.redirect('/');
+});
+
+router.get('/logout', function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log(username);
+  req.session.username = null;
+  res.redirect('/');
 });
 
 module.exports = router;
