a23320befb299c299cacab9852ca8f512c7f5d29
tajpure <tajpure@gmail.com>
2015-08-01 19:45:44 +0800

    stash

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ var config = {};
 config.local = false;
 config.username = 'admin';
 config.password = 'admin';
-config.base_dir = '/home/taojx/workspace/demo_blog';
+config.base_dir = '/home/tajpure/workspace/demo_blog';
 
 module.exports = config;
