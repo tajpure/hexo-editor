3d3bad5d2bcac9c66aed739136278cb16b0bd282
taojiaxuan <taojiaxuan@hoolai.com>
2016-05-05 18:41:56 +0800

    add replace

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const eslint = require('gulp-eslint');
 const cleanCSS = require('gulp-clean-css');
 const merge = require('merge-stream');
 const hash = require('gulp-hash');
+const replace = require('gulp-replace-task');
 
 gulp.task('scripts', () => {
   return gulp.src([
@@ -17,6 +18,7 @@ gulp.task('scripts', () => {
   ]).pipe(concat('app.js'))
     .pipe(uglify())
     .pipe(hash())
+    .pipe(hash.manifest('scripts.json'))
     .pipe(gulp.dest('public/dist'));
 });
 
@@ -27,7 +29,31 @@ gulp.task('css', () => {
   ]).pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(concat('style.css'))
     .pipe(hash())
+    .pipe(hash.manifest('css.json'))
     .pipe(gulp.dest('public/dist'));
+});
+
+gulp.task('replace', () => {
+  const cssJson = require('./public/dist/css.json');
+  const scriptsJson = require('./public/dist/scripts.json');
+  return gulp.src('views/layout.jade')
+    .pipe(replace({
+      patterns: [
+        {
+          match: 'style.css',
+          replacement: cssJson['style.css']
+        }
+      ]
+    }))
+    .pipe(replace({
+      patterns: [
+        {
+          match: 'app.js',
+          replacement: scriptsJson['app.js']
+        }
+      ]
+    }))
+    .pipe(gulp.dest('views'));
 });
 
 gulp.task('lint', () => {
